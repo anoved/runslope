edb675f080ddf66dc49005e90e78f978d1cee5df
Jim DeVona <jim.devona@gmail.com>
2015-01-30 10:58:14 -0500

    Collision handling and scale bar lines Collision handling is much simpler than original sg's algorithm... but works.

--- pysg.py
+++ pysg.py
@@ -4,16 +4,16 @@ import csv
 import re
 
 from pysvg.shape import *
-
 from pysvg.structure import *
 from pysvg.style import *
 from pysvg.text import *
 from pysvg.builders import StyleBuilder
 
 config = {
-	'vscale': 2,
+	'vscale': 1.25,
 	'fontsize': 9,
-	'linespan': 120
+	'linespan': 120,
+	'minvgap': 7
 }
 
 # List of dicts with keys: RACE, NAME, TIME, SECONDS
@@ -35,6 +35,9 @@ with open('freeze.csv', 'rb') as f:
 	for row in reader:
 		row['SECONDS'] = seconds(row['TIME'])
 		data.append(row)
+
+# optionally take only those that are present for all races (hardcoded == 4 hack)
+#data = filter(lambda q: len(filter(lambda k: k['NAME'] == q['NAME'], data)) == 4, data)
 
 # first, sort by time for bounds checking
 data.sort(key=lambda row: row['SECONDS'])
@@ -65,6 +68,7 @@ for i in range(1, len(data)):
 # next, sort by RACE, then SECONDS, then NAME (basically, restore input format)
 # could probably sort and filter at once with one list expression...
 data.sort(key=lambda rec: (rec['RACE'], rec['SECONDS'], rec['NAME']))
+
 races = []
 for key in sorted(racekeys):
 	results = filter(lambda result: result['RACE'] == key, data)
@@ -89,10 +93,8 @@ for key in sorted(racekeys):
 		'results': results
 	})
 
-
 svg_file = svg()
 
-# label styles
 svg_style = StyleBuilder()
 svg_style.setFontFamily(fontfamily="monospace")
 svg_style.setFontSize('9px')
@@ -107,8 +109,8 @@ svg_fadeline.setStrokeWidth(0.5)
 svg_fadeline.setStroke('#ddd')
 
 svg_scalestyle = StyleBuilder()
-svg_scalestyle.setStrokeWidth(4)
-svg_scalestyle.setStroke('#eed')
+svg_scalestyle.setStrokeWidth(6)
+svg_scalestyle.setStroke('#eef')
 
 svg_labels = g()
 svg_labels.set_style(svg_style.getStyle())
@@ -123,8 +125,6 @@ svg_flines.set_style(svg_fadeline.getStyle())
 svg_scale = g()
 svg_scale.set_style(svg_scalestyle.getStyle())
 
-# could have a master label group that contains each race group
-
 for r in range(0, len(races)):
 	
 	# left and right positions of race results - could calc in earlier loop
@@ -136,78 +136,64 @@ for r in range(0, len(races)):
 	#  to specify *intended* label width, and see if the browser/layout engine
 	#  will automatically fudge the text dimensions satisfactorily for alignment
 	
-	# label group
 	svg_lgroup = g()
-	#svg_lgroup.set_style(svg_style.getStyle())
-	#svg_lgroup.setAttribute("xml:space", "preserve")
 	
 	for i in range(0, len(races[r]['results'])):
 		
 		rec = races[r]['results'][i]
-		label =  races[r]['label_format'] % (rec['RANK'], rec['NAME'], rec['TIME'])
 		
 		# value we have to position
-		y = rec['SECONDS'] - mins
+		y = config['vscale'] * (rec['SECONDS'] - mins)
+		
+		# push collisions downward
+		if i > 0 and y - races[r]['results'][i-1]['y'] < config['minvgap']:
+			y = races[r]['results'][i-1]['y'] + config['minvgap']
+		
 		races[r]['results'][i]['y'] = y
 		
 		# draw result label
+		label =  races[r]['label_format'] % (rec['RANK'], rec['NAME'], rec['TIME'])
 		svg_label = text(label, races[r]['xl'], y)
 		svg_lgroup.addElement(svg_label)
 		
-		# and p == r to limit to one level
-		# (or p >= r-1 to limit to two levels, etc)
 		p = r
 		while p > 0:
 			p -= 1
+			
+			# look for results with the same name in this previous race
 			matches = filter(lambda cand: cand['NAME'] == rec['NAME'], races[p]['results'])
+			
+			# if a match is found, draw a link and stop looking for matches
 			if len(matches) == 1:
-				m = matches[0]
-				#print (p, m['y'])
 				
-				svg_link = line(races[p]['xr']-5, m['y']-3, races[r]['xl']+5, y-3)
+				svg_link = line(
+					races[p]['xr'] - 5,
+					matches[0]['y'] - 3,
+					races[r]['xl'] + 5,
+					y - 3)
 				
 				if p == r - 1:
+					# links to the immediately previous race are emphasized
 					svg_llines.addElement(svg_link)
 				else:
+					# links to earlier races are drawn in a separate group
 					svg_flines.addElement(svg_link)
-				
-				# draw link from
-				# (races[p][xr], m[y]) to (races[r][xl], y)
-				
-				# style link according to difference between p & r
-				# if p < r-1, this is a different category.
 				
 				break
 	
 	svg_labels.addElement(svg_lgroup)
 
-# scale gen. start with first minute before min result time
-
+# scale bars every minute from before first to after last finisher
+start_s = (int(mins)/60) * 60
+end_s = 120 + ((int(maxs)/60) * 60)
+for s in range(start_s, end_s, 60):
+	y = config['vscale'] * (s - mins)
+	scaleline = line(0, y, races[-1]['xr'], y)
+	svg_scale.addElement(scaleline)
 
 svg_file.addElement(svg_scale)
 svg_file.addElement(svg_flines)
 svg_file.addElement(svg_llines)
 svg_file.addElement(svg_labels)
 svg_file.save('./test.svg')
-
-
-#for i in range(0, len(data)-1):
-#	d = data[i]
-	
-	# 1 check for new race id & start new column & svg group for labels
-	
-	# 2 duplicate/close call adjustments (todo; as yet ignored)
-	
-	# 3 calculate vertical position based on seconds value (& any adjustments from above)
-	
-	# 4 format label (for now as a single string in fixed-width font)
-	
-	# 5 push result (& calculated positions) into temp array for this race
-	
-	# 6. at end of race set (inc. end of all results), loop through this race's
-	# result array and look for any name matches in previous race. If found,
-	# connect with a slopegraph line.
-	# todo: if not found, step back through *earlier* races and connect with
-	#       a muted background slopegraph line. Requires full results by race
-	#       rather than flat result list and temp race arrays.
 
