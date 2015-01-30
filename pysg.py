1221637dfd85b586f7c85b11f85875dc276ce91f
Jim DeVona <jim.devona@gmail.com>
2015-01-29 23:21:01 -0500

    Further partial draft Includes loop to identify link line targets. Does not yet nudge labels to adjust for collisions

--- pysg.py
+++ pysg.py
@@ -3,9 +3,12 @@
 import csv
 import re
 
+
 config = {
+	'vscale': 1,
 	'fontsize': 9,
-	'linespan': 50
+	'linespan': 50,
+	'margin': 10
 }
 
 # List of dicts with keys: RACE, NAME, TIME, SECONDS
@@ -34,11 +37,18 @@ data.sort(key=lambda row: row['SECONDS'])
 # min and max second value
 mins = data[0]['SECONDS']
 maxs = data[-1]['SECONDS']
+srange = maxs - mins
 
 # min and max deltas between neighborhing values
-mind = maxs - mins
+mind = srange
 maxd = 0
-for i in range(1, len(data)-1):
+
+# unique race ids
+racekeys = []
+
+for i in range(1, len(data)):
+	if data[i]['RACE'] not in racekeys:
+		racekeys.append(data[i]['RACE'])
 	diff = abs(data[i]['SECONDS'] - data[i-1]['SECONDS'])
 	if diff != 0:
 		if diff < mind:
@@ -46,53 +56,78 @@ for i in range(1, len(data)-1):
 		if diff > maxd:
 			maxd = diff
 
-print("Fastest time:", mins)
-print("Slowest time:", maxs)
-
-print("Min gap:", mind)
-print("Max gap:", maxd)
 
 # next, sort by RACE, then SECONDS, then NAME (basically, restore input format)
+# could probably sort and filter at once with one list expression...
 data.sort(key=lambda rec: (rec['RACE'], rec['SECONDS'], rec['NAME']))
-
-# list unique names and times
-unames = [data[0]['NAME']]
-utimes = [data[0]['SECONDS']]
-nraces = 1
-for i in range(1, len(data)-1):
-	# add new names to unique names list
-	if data[i]['NAME'] not in unames:
-		unames.append(data[i]['NAME'])
-	# add new times to unique times list
-	if data[i]['SECONDS'] not in utimes:
-		utimes.append(data[i]['SECONDS'])
-	# increment race count on race id change
-	if data[i-1]['RACE'] != data[i]['RACE']:
-		nraces += 1
-
-# I think it might actually be more useful at this point
-# to break the data into a dict of result lists, with one
-# for each races. Think it might simplify graph loop.
-
-# todo: find max char count of names, times, and rank (results per race)
-#       in order to calculate label column width. Char counts should be
-#       calculated on a per-race basis so each race column is best fitted
-
-# use unique 'RACE' values as keys for sorted result lists:
-# {'1': [{'NAME': 'Bob', 'SECONDS': 3423}, ...], '2': [...], ...}
-
-# then, instead of conditional context switching blocks within
-# a loop of all results, we'll have a more structured pair of loops:
-# the outer loop goes through races (only a few), and the inner loop
-# goes through sorted results from that race. 
-#
-# Simplifies #1 & #6 conditions below.
-# Still have to do #2, #3, and #4 as usual
-# No need for temp arrays used in #5 & #6;
-#  just push calculated values into the same data obj
+races = []
+for key in sorted(racekeys):
+	results = filter(lambda result: result['RACE'] == key, data)
+	
+	for n in range(0, len(results)):
+		results[n]['RANK'] = n + 1
+	
+	# result count per race (for char count for column)
+	resultcount = len(results)
+	mc_rank = len("%d" % resultcount)
+	
+	# super clumsiest way to do this
+	ns = sorted(results, key=lambda rec: len(rec['NAME']), reverse=True)
+	mc_name = len(ns[0]['NAME'])
+	
+	ts = sorted(results, key=lambda rec: (len(rec['TIME']), rec['SECONDS']), reverse=True)
+	mc_time = len(ts[0]['TIME'])
+	
+	races.append({
+		'wmax_label': mc_rank + 2 + mc_name + 1 + mc_time,
+		'label_format': '%' + str(mc_rank) + 'd. %-' + str(mc_name) + 's %' + str(mc_time) + 's',
+		'results': results
+	})
 
 
-# graph!
+for r in range(0, len(races)):
+	
+	# left and right positions of race results - could calc in earlier loop
+	races[r]['xl'] = (
+		config['margin']
+		+ (r * config['linespan'])
+		+ sum([(races[pr]['wmax_label'] * config['fontsize']) for pr in range(0, r - 1)])
+	)
+	races[r]['xr'] = races[r]['xl'] + (races[r]['wmax_label'] * config['fontsize'])
+	
+	for i in range(0, len(races[r]['results'])):
+		rec = races[r]['results'][i]
+		label =  races[r]['label_format'] % (rec['RANK'], rec['NAME'], rec['TIME'])
+		
+		# value we have to position
+		v = rec['SECONDS'] - mins
+		y = config['margin'] + v
+		
+		races[r]['results'][i]['y'] = y
+		
+		print (label, y)
+		
+		# draw label at (xl, y)
+		
+		p = r
+		while p > 0:
+			p -= 1
+			matches = filter(lambda cand: cand['NAME'] == rec['NAME'], races[p]['results'])
+			if len(matches) == 1:
+				m = matches[0]
+				print (p, m['y'])
+				
+				# draw link from
+				# (races[p][xr], m[y]) to (races[r][xl], y)
+				
+				# style link according to difference between p & r
+				# if p < r-1, this is a different category.
+				
+				break
+
+
+
+
 #for i in range(0, len(data)-1):
 #	d = data[i]
 	
@@ -113,7 +148,3 @@ for i in range(1, len(data)-1):
 	#       a muted background slopegraph line. Requires full results by race
 	#       rather than flat result list and temp race arrays.
 
-
-# for output, preferably stash relevant coordinates and strings in results dict,
-# but defer SVG generation for a separate pass - to keep styling and different
-# output format possibilities separate from the main layout loop.
